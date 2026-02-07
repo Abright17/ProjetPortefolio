085b8986718a72d873c1808346f98794373cb7ec
Abright17 <samuelamoussango@gmail.com>
2026-02-07 06:19:39 -0500

    index #1

---
+++ monsitePHP/index.php
@@ -0,0 +1,89 @@
+<?php include "header.php"; ?>
+<body>
+    <div>
+      <a href="galerie-mobile.html" class="expo">Découverte culinaire</a>
+    </div>
+    <div>
+      <a href="paradestination.html" class="ville"> Nos destinations </a>
+    </div>
+    <div><a href="glider.html" class="picture"> zone paradisiaque</a></div>
+    <div id="evenement"></div>
+    <div class="event">
+      <h1>Visite gratuite au Mont Kilimandjaro dans:</h1>
+    </div>
+
+    <h1>Accueil</h1>
+    <br />
+    <p>
+      L'Afrique est un continent qui est bordé le long de ses 26 000 km de côtes
+      par la mer Méditerranée, la mer Rouge, l’océan Indien et l’océan
+      Atlantique. Elle recouvre cinqs régions en tout: l'Afrique du Nord,
+      l'Afrique de l'Ouest,l'Afrique de l'Est, l'Afrique centrale, l'Afrique du
+      Sud(l'Afrique australe).L'Afrique regorge de diverses cultures dans chaque
+      pays et dans chaque ville.
+    </p>
+
+    <p>
+      Ce site a pour but de présenter les différentes cultures qu'on peut
+      retrouver dans l'Afrique du Nord,l'Afrique centrale, et dans l'Afrique du
+      Sud(l'Afrique australe).
+    </p>
+    <div class="container">
+      <div class="images">
+        <a href="afrique-du-nord.html"
+          ><img
+            src="decoration/imagesdecoratives/layalger.jpg"
+            title="Poterie Algerienne"
+            alt=" poterie algerie"
+            class="card-image"
+          />
+        </a>
+        <a href="decoration/imagesdecoratives/layalger.jpg" class="title"
+          ><h3>Poterie Algérienne</h3></a
+        >
+        <p>
+          La poterie algérienne en Algérie est considérée comme un patrimoine
+          culturel et un guide culturel en Algérie, en particulier dans un
+          certain nombre de ses villes intérieures et côtières, où elle est
+          devenue une destination touristique sans visa
+        </p>
+      </div>
+
+      <div class="images">
+        <a href="afrique-centrale.html"
+          ><img
+            src="decoration/imagesdecoratives/ngondo.jpg"
+            title="Festivale Ngondo au Cameroun"
+            alt="ngondo"
+            class="card-image"
+          />
+        </a>
+        <a href="decoration/imagesdecoratives/ngondo.jpg" class="title"
+          ><h3>Festivale Ngondo au Cameroun</h3></a
+        >
+        <p>
+          Le Festivale Ngondo prend place au Cameroun au bord du fleuve Wouri
+          situé à Douala la capitale économique
+        </p>
+      </div>
+
+      <div class="images">
+        <a href="afrique-du-Sud.html"
+          ><img
+            src="decoration/imagesdecoratives/masquezambie.jpg"
+            title="masque zambien"
+            alt="Masques zambiens"
+            class="card-image"
+          />
+        </a>
+        <a href="decoration/imagesdecoratives/masquezambie.jpg" class="title"
+          ><h3>Masques Zambiens</h3></a
+        >
+        <p>
+          Les Masques zambiens sont des masques de protection divine et des
+          emblèmes de la culture Zambiennes
+        </p>
+      </div>
+    </div>
+  </body>
+<?php include "footer.php"; ?>
